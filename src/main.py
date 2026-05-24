71e559bbb0996d162c781b4493aafd490308c955
Benjamin Su <benjaminsumacbookm4@Winstons-MacBook-Air.local>
2026-05-24 00:34:45 -0700

    First commit

---
+++ src/main.py
@@ -0,0 +1,18 @@
+def hello_world():
+    print("Hello world!")
+
+def add_numbers(a, b):
+    sum = a + b
+    return sum;
+
+def divide_numbers(dividend, divisor):
+    quotient = dividend/divisor
+    return quotient
+
+def multiply_numbers(a, b):
+    product = a * b
+    return product
+
+def subtract_numbers(minuend, subtrahend): # minuend = starting number, subtrahend = number you are subracting from minuend
+    difference = minuend - subtrahend
+    return difference
